f9751510b7a1e1a3ad24088a0de5df1707577c17
Mouse <mouse008@gmail.com>
2018-09-23 14:49:05 -0400

    Enhance the auto-builder

--- cryfs-conf-build.sh
+++ cryfs-conf-build.sh
@@ -11,15 +11,24 @@ rm -rf build
 mkdir -p build
 cd build
 
+# Normal build on MacOS
+CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off"
+# Self-explanatory - build on MacOS without OpenMP (useful when neither Macports nor Brew is there)
 #CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off -DDISABLE_OPENMP=ON"
-CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off -DCMAKE_C_COMPILER=clang-mp-6.0 -DCMAKE_CXX_COMPILER=clang++-mp-6.0"
+# Build on MacOS, enforcing Macports-installed Clang (currently not necessary, kept for historic reasons)
+#CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off -DCMAKE_C_COMPILER=clang-mp-6.0 -DCMAKE_CXX_COMPILER=clang++-mp-6.0"
 
+# Installation prefix compatible with where Macports installs stuff on MacOS
 CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_INSTALL_PREFIX=/opt/local"
 
-CMAKEFLAGS="${CMAKEFLAGS} -DBoost_USE_STATIC_LIBS=off -DCRYPTOPP_LIB_PATH=/opt/local/lib -DCRYFS_UPDATE_CHECKS=off"
+# Ensure cryfs does not try to reach over Internet for updates
+CMAKEFLAGS="${CMAKEFLAGS} -DBoost_USE_STATIC_LIBS=off -DCRYFS_UPDATE_CHECKS=off"
+
+# Provide verbose output - useful to debug build process
+#CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
 cmake .. ${CMAKEFLAGS} -DCMAKE_C_FLAGS="-I/opt/local/include"
-make -j 4
+make -j 4 V=1
 #make check
 cd ..
 exit 0
