6784daf65637d07611e02b513f43de5c39d3e9c1
Mouse <mouse008@gmail.com>
2018-09-24 14:55:38 -0400

    Added a lot of options to auto-builder script to accommodate various ways of including OpenMP, and to produce verbose build output

--- cryfs-conf-build.sh
+++ cryfs-conf-build.sh
@@ -12,12 +12,13 @@ mkdir -p build
 cd build
 
 # CMake flags needed for a normal build on MacOS
-CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off"
+CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off "
 
 ############################################################
 # The following section deals with various ways to configure
 # OpenMP on MacOS using Clang from Xcode or installed by
-# Macports. You need to uncomment only one of those lines.
+# Macports. You need to uncomment only one of those options,
+# or none if the above default works (and finds libomp).
 #
 # If instead of Macports you use Brew, change the absolute paths
 # for libomp to what they should be for Brew.
@@ -29,8 +30,13 @@ CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off"
 # Build on MacOS, enforcing Macports-installed Clang
 #CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_C_COMPILER=clang-mp-6.0 -DCMAKE_CXX_COMPILER=clang++-mp-6.0"
 #
-# Using AppleClang from Xcode with Macports-installed libomp
-CMAKEFLAGS="${CMAKEFLAGS} -DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I/opt/local/include/libomp'" -DOpenMP_CXX_LIB_NAMES=omp -DOpenMP_omp_LIBRARY=/opt/local/lib/libomp.dylib"
+# Using AppleClang from Xcode with Macports-installed libomp (currently does not work)
+OMP_CMAKE_C_FLAGS="-DOpenMP_C_FLAGS=-I/opt/local/include/libomp"
+OMP_CMAKE_CXX_FLAGS="-DOpenMP_CXX_FLAGS=-I/opt/local/include/libomp"
+OMP_CMAKE_OMP="-Xclang -fopenmp"
+OMP_CMAKE_C_LIBNAMES="-DOpenMP_C_LIB_NAMES=omp"
+OMP_CMAKE_CXX_LIBNAMES="-DOpenMP_CXX_LIB_NAMES=omp"
+CMAKEFLAGS="${CMAKEFLAGS} ${OMP_CMAKE_C_FLAGS} ${OMP_CMAKE_CXX_FLAGS} ${OMP_CMAKE_C_LIBNAMES} ${OMP_CMAKE_CXX_LIBNAMES} -DOpenMP_omp_LIBRARY=/opt/local/lib/libomp/libomp.dylib"
 ############################################################
 
 
@@ -41,9 +47,9 @@ CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_INSTALL_PREFIX=/opt/local"
 CMAKEFLAGS="${CMAKEFLAGS} -DBoost_USE_STATIC_LIBS=off -DCRYFS_UPDATE_CHECKS=off"
 
 # Provide verbose output - useful to debug build process
-#CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+CMAKEFLAGS="${CMAKEFLAGS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
-cmake .. ${CMAKEFLAGS} -DCMAKE_C_FLAGS="-I/opt/local/include"
+cmake .. ${CMAKEFLAGS} -DCMAKE_C_FLAGS="-I/opt/local/include" -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OMP_CMAKE_OMP} ${OMP_CMAKE_CXX_FLAGS}"
 make -j 4 V=1
 #make check
 cd ..
